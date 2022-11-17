e5d22e44dbd626803792de32c067c66d2dcdd27c
Zrzla22 <114632152+Zrzla22@users.noreply.github.com>
2022-11-17 07:16:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+==> Code In Termux <==
+pkg install update && pkg upgrade 
+pkg install lolcat figlet toilet cowsay -y
+pkg install zsh -y
+zsh 
+
+==> Code In Linux <==
+apt-get update && apt-get upgrade 
+Apr-get install lolcat figlet toilet cowsay -y
+apt-get install zsh -y
+zsh
